8a791519e2e21e17803ae6ea78f0107284e160bf
MiNote <kovalenko_evgeny@bk.ru>
2020-07-08 20:32:22 +0300

    Сделал CRUD для первой колонки: физ.лицо

---
+++ divorce/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Fiz_l
+
+class Fiz_l_form(forms.ModelForm):
+    class Meta:
+        model = Fiz_l
+        # можно fields = '__all__'
+        fields = ('name', 'date_of_birth', 'sex')
+        labels = {
+            'name': 'Имя',
+            'date_of_birth': 'Дата рождения',
+            'sex': 'Пол'
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(Fiz_l_form, self).__init__(*args, **kwargs)
+        # убрать обязательность поля
+        #self.fields['sex'].required = False
--- divorce/urls.py
+++ divorce/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("divorce/", DivorceView.as_view()),
     path('divorce/add_person/', add_person, name='add_person'),
     path('divorse/del_person/<int:person_id>/', del_person, name='delete_person'),
-    path('divorce/form_fiz_l/', fiz_l_form_add, name='form_add')
+    path('divorce/form_fiz_l/', fiz_l_form_add, name='form_add'),
+    path('divorce/form_fiz_l/<int:id>/', fiz_l_form_add, name='person_update')
 ]
