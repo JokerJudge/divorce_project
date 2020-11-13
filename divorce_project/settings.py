b4204a9fce378469a8f0348dbad2e9258df725ed
MiNote <kovalenko_evgeny@bk.ru>
2020-11-13 16:31:25 +0300

    Работающий на nginx через докер сервер c работающими статик-файлами

--- divorce_project/settings.py
+++ divorce_project/settings.py
@@ -145,7 +145,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+# для dev
+#STATIC_URL = '/static/'
+
+#для prod
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # в консоль
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
