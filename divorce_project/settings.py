c6d9d7b79b1887b6e7a511b3f2e5fc01dd31365b
MiNote <kovalenko_evgeny@bk.ru>
2020-11-18 22:51:40 +0300

    Подстройка для dev и для prod для комфортного переключения

--- divorce_project/settings.py
+++ divorce_project/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'divorce_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_USER'),
+        'NAME': os.environ.get('POSTGRES_NAME'), # было POSTGRES_USER
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': os.environ.get('SQL_HOST'),
